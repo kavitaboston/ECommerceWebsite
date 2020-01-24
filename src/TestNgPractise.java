3c659cd3006f8f4318abccd55e72dbee09771f16
kavitaboston <57677152+kavitaboston@users.noreply.github.com>
2020-01-24 13:58:01 -0500

    initial commit

---
+++ src/TestNgPractise.java
@@ -0,0 +1,16 @@
+import org.testng.annotations.Test;
+
+public class TestNgPractise {
+
+    @Test
+    public void Demo()
+    {
+        System.out.println("Hello");
+    }
+    @Test
+    public void DemoTwo()
+    {
+        System.out.println("Demo2");
+    }
+
+}
